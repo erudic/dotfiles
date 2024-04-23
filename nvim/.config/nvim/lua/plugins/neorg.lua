ca5bb266a6f676a3f6017f144a69a1a4c6b5175b
gendo <e.rudic@gmail.com>
2024-04-23 19:10:08 +0200

    feat(nvim): kanagawa and stuff

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,25 +1,29 @@
 return {
-  -- "nvim-neorg/neorg",
-  -- dependencies = { "nvim-lua/plenary.nvim" },
-  -- build = ":Neorg sync-parsers",
-  -- -- tag = "*",
-  -- lazy = true, -- enable lazy load
-  -- ft = "norg", -- lazy load on file type
-  -- cmd = "Neorg", -- lazy load on command
-  -- config = function()
-  --   require("neorg").setup({
-  --     load = {
-  --       ["core.defaults"] = {}, -- Loads default behaviour
-  --       ["core.concealer"] = {}, -- Adds pretty icons to your documents
-  --       ["core.dirman"] = { -- Manages Neorg workspaces
-  --         config = {
-  --           workspaces = {
-  --             notes = "~/notes/lb",
-  --             private = "~/notes/pr",
-  --           },
-  --         },
-  --       },
-  --     },
-  --   })
-  -- end,
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release  -- put any other flags you wanted to pass to lazy here!
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes/lb",
+                private = "~/notes/pr",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
